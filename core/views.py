6945f55830545e5d7038ef4cfbe8240a441e16d1
retech <itsregalo047@gmail.com>
2021-09-30 22:40:23 +0300

    about us page

--- core/views.py
+++ core/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import messages
+from django.core.mail import send_mail
 # Create your views here.
 
 
 def IndexView(request, *args, **kwargs):
     return render(request, 'index.html')
+
+
+def ContactUs(request, *args, **kwargs):
+    if request.method == 'POST':
+        name = request.POST['first_name']
+        email = request.POST['your-email']
+        subject = request.POST['subject']
+        message = request.POST['your-message']
+        
+        if name == "":
+            messages.error(request, "Name is required")
+            if email == "":
+                messages.error(request, "email is required")
+                if subject == "":
+                    messages.error(request, "subject is reuired")
+                    if messages == "":
+                        messages.error(request, "Message is required")
+                        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+                        
+        send_mail(subject,message, email,["contact@greengarnetsfilms.com"],fail_silently=True)
+        messages.success(request, "Thank you for your feedback")
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    return render(request, 'contact-us.html')
