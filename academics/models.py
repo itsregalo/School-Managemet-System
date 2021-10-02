4179a901d96f5dd9d27f62bbc0e15813eecf9e30
retech <itsregalo047@gmail.com>
2021-10-02 21:52:33 +0300

    academics app

--- academics/models.py
+++ academics/models.py
@@ -14,3 +14,7 @@ class Teachers(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     subject = models.ForeignKey('Subject', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
