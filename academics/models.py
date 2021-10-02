6b08f5d63bedfa702452382c22071d12300dc9f3
retech <itsregalo047@gmail.com>
2021-10-02 21:39:07 +0300

    academics app

--- academics/models.py
+++ academics/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=50)
+    code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
