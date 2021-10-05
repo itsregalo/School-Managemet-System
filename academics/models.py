be3d4776b8333bc5db7d46ccd4fb2b7a00659a8f
retech <itsregalo047@gmail.com>
2021-10-05 21:24:05 +0300

    academics

--- academics/models.py
+++ academics/models.py
@@ -1,7 +1,7 @@
 from django.db import models
+from django.db.models.fields.related import ForeignKey
 from django.utils.text import slugify
 # Create your models here.
-
 
 class Department(models.Model):
     name = models.CharField(max_length=50)
@@ -14,6 +14,7 @@ class Teacher(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     subjects = models.ManyToManyField('Subject')
+    tsc_no = models.CharField(max_length=50)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
@@ -30,5 +31,17 @@ class Subject(models.Model):
 
 class Class(models.Model):
     name = models.CharField(max_length=254)
+
+class ExamBank(models.Model):
+    name = models.CharField(max_length=254)
+    examination_date = models.DateTimeField()
+    class_level = models.ForeignKey(Class, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Results(models.Model):
+    pass
+    
 
     
