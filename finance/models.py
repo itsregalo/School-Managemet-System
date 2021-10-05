85bd191e3e1408f7515bb58ee4cad583f6da603e
retech <itsregalo047@gmail.com>
2021-10-05 21:36:55 +0300

    fees

--- finance/models.py
+++ finance/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.db.models.deletion import DO_NOTHING
+from academics.models import Class
 # Create your models here.
+
+
+class FeeStructure(models.Model):
+    class_name = models.ForeignKey(Class, on_delete=DO_NOTHING)
+    date = models.DateField()
+    file = models.FileField(upload_to=f'fee_structure/{class_name.name}/%Y')
+    approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.class_name.name} - {self.date}"
+    
--- studentportal/views.py
+++ studentportal/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render
+from admissions.models import Student
+from .models import *
 # Create your views here.
+
+
+def myPortal(request, *args, **kwargs):
+    student_profile = get_object_or_404(Student, user=request.user)
+
+    context = {
+        'student_profile':student_profile
+    }
+    return render(request, 'student-portal/portal.html', context)
