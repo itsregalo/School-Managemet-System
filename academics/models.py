1c34faf118a78a27ea55e7a81455dd57b2ab4720
retech <itsregalo047@gmail.com>
2021-10-08 22:03:03 +0300

    student class

--- academics/models.py
+++ academics/models.py
@@ -22,6 +22,18 @@ class AcademicTerm(models.Model):
 
     def __str__(self):
         return self.name
+
+class StudentClass(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+    class Meta:
+        verbose_name = "Class"
+        verbose_name_plural = "Classes"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
     
     
 class Department(models.Model):
