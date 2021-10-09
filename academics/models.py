645863dd514ceeda0c76bdab3475d6a17c53d0f6
retech <itsregalo047@gmail.com>
2021-10-09 18:08:18 +0300

    results

--- academics/models.py
+++ academics/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.fields.related import ForeignKey
 from django.utils.text import slugify
+from admissions.models import Student
 # Create your models here.
 
 
@@ -73,8 +74,25 @@ class ExamBank(models.Model):
     def __str__(self):
         return self.name
 
-class Results(models.Model):
-    pass
-    
+class Result(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    session = models.ForeignKey(AcademicSession, on_delete=models.CASCADE)
+    term = models.ForeignKey(AcademicTerm, on_delete=models.CASCADE)
+    current_class = models.ForeignKey(StudentClass, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    test_score = models.IntegerField(default=0)
+    exam_score = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ["subject"]
+
+    def __str__(self):
+        return f"{self.student} {self.session} {self.term} {self.subject}"
+
+    def total_score(self):
+        return self.test_score + self.exam_score
+
+    def grade(self):
+        return score_grade(self.total_score())
 
     
