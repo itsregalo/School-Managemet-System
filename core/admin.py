6e63b32c364ac9c1aa85b2040c111b64fd904156
retech <itsregalo047@gmail.com>
2021-10-09 23:20:43 +0300

    core admin

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Gallery, AcademicSession, AcademicTerm
+from .models import (Gallery, AcademicSession, AcademicTerm,
+                        StudentClass, NewsCategory, News, Event)
 # Register your models here.
 
 
 admin.site.register(Gallery)
 admin.site.register(AcademicSession)
 admin.site.register(AcademicTerm)
+admin.site.register(StudentClass)
