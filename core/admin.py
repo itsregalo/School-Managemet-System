8f790f9dbff21abb2e94ef7f84a55c25861e312b
retech <itsregalo047@gmail.com>
2021-10-09 23:18:51 +0300

    admission forms

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Gallery
+from .models import Gallery, AcademicSession, AcademicTerm
 # Register your models here.
 
 
 admin.site.register(Gallery)
+admin.site.register(AcademicSession)
+admin.site.register(AcademicTerm)
