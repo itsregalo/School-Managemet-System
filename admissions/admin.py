7d4f6a1ac31163c5654a311a00bfe7c646bb654d
retech <itsregalo047@gmail.com>
2021-10-13 23:46:42 +0300

    utilised import export on admin

--- admissions/admin.py
+++ admissions/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import *
-# Register your models here.
+from import_export.admin import ImportExportModelAdmin
 
+@admin.register(Student)
+class StudentAdmin(ImportExportModelAdmin):
+    list_display = ['admission_no','first_name','last_name','gender','in_class']
 
-admin.site.register(Student)
 admin.site.register(Parent)
 admin.site.register(Staff)
 admin.site.register(Teacher)
