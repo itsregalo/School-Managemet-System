ec5e9bedc18127973f146553e94eb49ee64b47d2
retech <itsregalo047@gmail.com>
2021-10-14 10:46:50 +0300

    removed emptry dirs

--- academics/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-
-# Create your views here.
-def UploadResults(request, *args, **kwargs0 ):
-    pass
--- admissions/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import StudentList
-app_name = 'admissions'
-
-urlpatterns = [
-    path('students/', StudentList, name='student-list')
-]
