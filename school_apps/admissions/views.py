3f92e43925e775bbd7f7e447d8b976637b608551
retech <itsregalo047@gmail.com>
2021-10-16 22:50:54 +0300

    upload file via excel

--- school_apps/admissions/views.py
+++ school_apps/admissions/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import login_required
 from admissions.models import Student
-
-# Create your views here.
+from resources import StudentResource
+from tablib import Dataset
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def StudentList(request, *args, **kwargs):
     students = Student.objects.filter(has_cleared=False)
@@ -22,20 +25,20 @@ def StudentDetail(request, pk, *args, **kwargs):
 @login_required
 def add_contact_via_file(request, *args, **kwargs):
         
-    contacts = Contact.objects.all()
+    students = StudentResource.objects.all()
     
     if request.method == 'POST':        
-        contact_resourse = ContactResource()
+        student_resourse = StudentResource()
         dataset = Dataset()
-        new_contact = request.FILES.get('contact-file')
+        new_student = request.FILES.get('student-file')
         
-        if not new_contact.name.endswith('xlsx'):
+        if not new_student.name.endswith('xlsx'):
             messages.error(request, 'The file must be in excel format')
-            return render(request, 'app-contact.html')
+            return render(request, 'admin-student-list.html')
         
-        imported_data = dataset.load(new_contact.read(), format='xlsx')
+        imported_data = dataset.load(new_student.read(), format='xlsx')
         for data in imported_data:
-            value = Contact(
+            value = Student(
                 data[0], 
                 data[1], 
                 data[2], 
@@ -46,7 +49,7 @@ def add_contact_via_file(request, *args, **kwargs):
             messages.success(request, "contacts saved")
     
     context = {
-        'contacts':contacts
+        'students':students
     }
     
     return HttpResponseRedirect(reverse('mainapp:contacts'))
