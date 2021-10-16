2a15b6e1f75dd3d4439d2b0349aaaa12bed66c2a
retech <itsregalo047@gmail.com>
2021-10-16 22:45:31 +0300

    upload file via excel

--- school_apps/academics/views.py
+++ school_apps/academics/views.py
@@ -1,6 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 # Create your views here.
 def UploadResults(request, *args, **kwargs0 ):
-    pass
+    return HttpResponse('Hello')
--- school_apps/admissions/views.py
+++ school_apps/admissions/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, render
-
+from django.contrib.auth.decorators import login_required
 from admissions.models import Student
 
 # Create your views here.
@@ -18,3 +18,35 @@ def StudentDetail(request, pk, *args, **kwargs):
         'student':student
     }
     return render(request, 'admission/sigle-student.html')
+
+@login_required
+def add_contact_via_file(request, *args, **kwargs):
+        
+    contacts = Contact.objects.all()
+    
+    if request.method == 'POST':        
+        contact_resourse = ContactResource()
+        dataset = Dataset()
+        new_contact = request.FILES.get('contact-file')
+        
+        if not new_contact.name.endswith('xlsx'):
+            messages.error(request, 'The file must be in excel format')
+            return render(request, 'app-contact.html')
+        
+        imported_data = dataset.load(new_contact.read(), format='xlsx')
+        for data in imported_data:
+            value = Contact(
+                data[0], 
+                data[1], 
+                data[2], 
+                data[3],
+                data[4]
+            )
+            value.save()
+            messages.success(request, "contacts saved")
+    
+    context = {
+        'contacts':contacts
+    }
+    
+    return HttpResponseRedirect(reverse('mainapp:contacts'))
