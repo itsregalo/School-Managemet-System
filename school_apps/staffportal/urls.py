2da2523dc90c0006f590e20fa082b787aad3f5e1
retech <itsregalo047@gmail.com>
2021-11-01 22:00:03 +0300

    staff routes

--- school_apps/staffportal/urls.py
+++ school_apps/staffportal/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from .views import *
 
-app_name = "staff"
+app_name = 'staffportal'
 
 urlpatterns = [
-    
+    path('login/', StaffLogin, name='staff-login'),
+    path('logout/', StaffLogOutView, name='staff-logout'),
+    path('me/', StaffPortal, name='my-staff-portal'),
 ]
