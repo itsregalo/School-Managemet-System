0cd9dea0b7a702e2b982b81fae5a560443953b3b
retech <itsregalo047@gmail.com>
2021-11-22 23:18:20 +0300

    utils

--- utilities/views.py
+++ utilities/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import *
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required
+def displayThumbnail(request, *args, **kwargs):
+    sliders = SliderImage.objects.all()
+    context = {
+        'sliders':sliders
+    }
+    return render('utils/sliders.html', context)
