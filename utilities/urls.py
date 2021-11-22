1f6ed74fec11ecb828e8e922c9810da7e3277341
retech <itsregalo047@gmail.com>
2021-11-22 23:22:17 +0300

    utils

--- utilities/urls.py
+++ utilities/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+
+from utilities.models import SliderImage
 from . import views
 
-app_name = 'utilities'
+app_name = 'utils'
 
 urlpatterns = [
-    
+    path('sliders/', SliderImage, name='sliders')
 ]
