d0b899681e35d26ecb9c95ff6a56484b460fb265
retech <itsregalo047@gmail.com>
2021-11-22 23:06:57 +0300

    utils pic

--- utilities/models.py
+++ utilities/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from imagekit.models.fields import ImageSpecField
+from pilkit.processors.resize import ResizeToFill
 
 # Create your models here.
+
+class SliderImage(models.Model):
+    name = models.CharField(max_length=254)
+    image = models.ImageField(upload_to='utils/images/%Y/%m')
+    image_thumbnail = ImageSpecField(source='image',
+                                    processors=[ResizeToFill(1800,1119)])
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
