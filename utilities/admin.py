1268abd0e1a662e7f049d9d8c0146e27fadf1a3c
retech <itsregalo047@gmail.com>
2021-12-12 21:32:18 +0300

    utils quicklonks

--- utilities/admin.py
+++ utilities/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import SliderImage
+from .models import SliderImagem, QuicklinkImages
 # Register your models here.
 
 class SliderImageAdmin(admin.ModelAdmin):
     list_display = ['name','image','image_thumbnail']
 
 admin.site.register(SliderImage, SliderImageAdmin)
+
+class QuickLinkAdmin(admin.ModelAdmin):
+    list_display = ['name','image','image_thumbnail']
+
+admin.site.register(QuicklinkImages, QuickLinkAdmin)
