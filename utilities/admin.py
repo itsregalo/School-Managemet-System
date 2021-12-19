dc770030a182b21dc7b9a1da58bea00d80904da4
retech <itsregalo047@gmail.com>
2021-12-19 22:54:23 +0300

    changes

--- utilities/admin.py
+++ utilities/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import SliderImage, QuicklinkImages
+from .models import (SliderImage, QuicklinkImages, IndexEvent400412,
+                        IndexWhyUs700493)
 # Register your models here.
 
 class SliderImageAdmin(admin.ModelAdmin):
@@ -11,3 +12,13 @@ class QuickLinkAdmin(admin.ModelAdmin):
     list_display = ['name','image','image_thumbnail']
 
 admin.site.register(QuicklinkImages, QuickLinkAdmin)
+
+class IndexEventAdmin(admin.ModelAdmin):
+    list_display = ['name','image','image_thumbnail']
+
+admin.site.register(IndexEvent400412, IndexEventAdmin)
+
+class IndexWhyUsAdmin(admin.ModelAdmin):
+    list_display = ['name','image','image_thumbnail']
+
+admin.site.register(IndexWhyUs700493, IndexWhyUsAdmin)
