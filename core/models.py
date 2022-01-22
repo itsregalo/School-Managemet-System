f8a038526e0426ad13cf5d169b5c72bdb79d80ba
retech <itsregalo047@gmail.com>
2022-01-22 20:43:13 +0300

    notice configs

--- core/models.py
+++ core/models.py
@@ -108,6 +108,7 @@ class Notice(models.Model):
                                    format='JPEG',
                                    options = {'quality':100})
     description = models.TextField(blank=True, null=True)
+    date_of_event = models.DateField(blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     news_file = models.FileField(upload_to='files/news/%Y/%m/%d', blank=True, null=True)
     slug = models.SlugField(blank=True)
